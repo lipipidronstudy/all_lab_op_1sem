1195a4b265fad15a622730ed460e4e8f3d5cf8bc
lipipidronstudy <114152213+lipipidronstudy@users.noreply.github.com>
2022-10-18 12:47:18 +0300

    lab12

---
+++ lab12/perehoddat.h
@@ -0,0 +1,15 @@
+//
+// Created by VadimAlg on 17.09.2022.
+//
+
+#ifndef LAB12_PEREHODDAT_H
+#define LAB12_PEREHODDAT_H
+int provmes (char s1, char s2, char s3);
+void nextmonth(char date[]);
+void dweek(char date[]);
+
+
+
+#endif //LAB12_PEREHODDAT_H
+
+
